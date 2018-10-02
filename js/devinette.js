1e281cc972b415aa351da904d06200b0f93676e2
CedricBour <cedric.bour@hotmail.fr>
2018-10-02 20:40:10 +0200

    [fix] Completing Game
    I have completed the game, it's working well now

--- js/devinette.js
+++ js/devinette.js
@@ -10,14 +10,46 @@ console.log("--------------------------------------------");
 
 // Cette ligne génère aléatoirement un nombre entre 1 et 100
 var solution = Math.floor(Math.random() * 100) + 1;
-var nbEssaisMax = 6;
 
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
-console.log("(La solution est " + solution + ")");
+//console.log("(La solution est " + solution + ")");
+
+/* VARIABLES */
+var maxAttemptsNumber = 6;
+var currentPlayerAttempt = 0;
+var playerNumber;
 
 console.log(""); 
 console.log("But du jeu : trouvez le bon entier entre 1 et 100 en moins de " +
-	nbEssaisMax + "essais".); 
+	maxAttemptsNumber + " essais"); 
 console.log(""); 
 console.log("--------------------------------------------");
 
+while(true) {
+
+	if(currentPlayerAttempt < maxAttemptsNumber) {
+
+		playerNumber = prompt("Entrez un nombre");
+		currentPlayerAttempt++;
+		if(playerNumber > solution ) {
+			console.log(playerNumber + " est trop grand. Essayez encore"); 
+		} 
+		else if(playerNumber < solution ) {
+			console.log(playerNumber + " est trop petit. Essayez encore"); 
+		} 
+		else if(playerNumber == solution ) {
+			console.log(""); 
+			console.log("La solution est bien " + solution); 
+			console.log("Félicitation, vous avez REUSSI !!!"); 
+			console.log("Rechargez la page pour rejouer avec nous!")
+		}
+		else {
+			console.log("Il y a eu une erreur, rechargez la page svp..."); 
+		}
+	}
+	else {
+	console.log("Vous n'avez plus d'essais !")
+	console.log("Rechargez la page pour réessayer !")
+	}
+
+}
